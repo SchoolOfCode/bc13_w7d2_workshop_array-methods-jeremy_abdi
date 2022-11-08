6595e41d761d67a012c4f15150ecf19aa6741bdd
Abdiqani Mohamed <Mohamedabdiqani321@gmail.com>
2022-11-08 14:04:08 +0000

    done task 2

--- task1.js
+++ task1.js
@@ -1,3 +1,8 @@
+// 👉 1a. Write a function that takes in a string and turns it to upper case.
+
+// 👉 1b. Use the `.map()` method to generate a new array by calling your function on each item in `cats`. Store the returned value from map in the variable `capitalisedCats`,
+
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map((cat)=>cat.toUpperCase())
+console.log(capitalisedCats) 
--- task2.js
+++ task2.js
@@ -1,3 +1,7 @@
+// You have an array of lovely animals. However, you can only keep the animals that start with the letter `b`. 
+
+// 👉 2a. In the new variable `bListAnimals`, use the `.filter()` method to generate a new array which contains only the animals whose name begins with 'b'. 
+
 let animals = [
     "baboon",
     "kangaroo",
@@ -8,4 +12,5 @@ let animals = [
     "basalisk",
   ];
   
-let bListAnimals = // Your 'filter' code here.
+let bListAnimals = animals.filter((animal)=>animal[0]==='b')
+console.log(bListAnimals)
